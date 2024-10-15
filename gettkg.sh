cdb7f656b670a1666f4c7b1d991b98b0aadb9186
hackernoodle <118927624+hackernoodle@users.noreply.github.com>
2024-10-15 21:38:18 +0300

    add wine-tkg

---
+++ gettkg.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+OWNER="Frogging-Family"
+REPO="wine-tkg-git"
+WORKFLOW_ID=11219483
+
+RUN_ID=`gh api \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "/repos/$OWNER/$REPO/actions/workflows/$WORKFLOW_ID/runs?status=success&per_page=1" \
+  --jq '.workflow_runs[0].id'`
+
+ARTIFACT_ID=`gh api \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "/repos/$OWNER/$REPO/actions/runs/$RUN_ID/artifacts" \
+  --jq '.artifacts[0].id'`
+
+gh api \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "/repos/$OWNER/$REPO/actions/artifacts/$ARTIFACT_ID/zip" \
+  > wine-tkg.zip
+
+unzip wine-tkg.zip
